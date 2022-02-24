8822dba7f36f784d1142e47c283a844339635e92
nareshban <nareshban40@gmail.com>
2022-02-24 11:55:08 +0545

    feat: Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,36 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ReactGA from "react-ga4";
+import { Link, Route, Routes } from "react-router-dom";
 
 ReactGA.initialize("G-WJ9JJTMN1V");
 ReactGA.send("pageview");
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>HOME PAGE</p>
+          </header>
+        </Route>
+        <Route path="/about">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>ABOUT PAGE</p>
+          </header>
+        </Route>
+        <Route path="/contact">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>CONTACT PAGE</p>
+          </header>
+        </Route>
+      </Routes>
+      <Link to={"/"}>HOME</Link>
+      <Link to={"/about"}>ABOUT</Link>
+      <Link to={"/contact"}>CONTACT</Link>
     </div>
   );
 }
