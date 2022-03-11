af9182466714d8565fca8a09cbca2fc6d546ca0b
nareshban <nareshban40@gmail.com>
2022-03-11 17:42:17 +0545

    feat: even and dimension send at the same time

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import ReactGA from "react-ga";
+import {
+  GAEventActions,
+  GAEventCategory,
+  GAEventDetails,
+  GAEventLabels,
+} from "../helpers/enums";
 const Login = () => {
   const navigate = useNavigate();
 
@@ -16,16 +22,35 @@ const Login = () => {
   }, [isLoggedIn]);
 
   const signInClick = async () => {
-    await signInGoogle().then((response: any) => {
-      console.log(response);
-      ReactGA.event({
-        category: "Logged In",
-        action: "Logged In using Google Account",
-        label: `${response?.user?.displayName} has Logged In`,
+    await signInGoogle()
+      .then((response: any) => {
+        console.log(response);
+        // ReactGA.event({
+        //   category: "Logged In",
+        //   action: "Logged In using Google Account",
+        //   label: `${response?.user?.displayName} has Logged In`,
+        // });
+        // ReactGA.set({ dimension2: response?.user?.uid });
+
+        ReactGA.ga(
+          "send",
+          "event",
+          GAEventCategory.LOGIN,
+          `${response?.user?.displayName} has Logged In`,
+          GAEventActions.LOGIN_CLICKED,
+          {
+            dimension2: response?.user?.uid,
+          }
+        );
+        navigate("/");
+      })
+      .catch((error: any) => {
+        ReactGA.exception({
+          description: "An error ocurred" + error.message,
+          fatal: true,
+        });
+        navigate("/login");
       });
-      ReactGA.set({ dimension2: response?.user?.uid });
-      navigate("/");
-    });
   };
   return <button onClick={signInClick}>Login with Google</button>;
 };
